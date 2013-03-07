7b57da5dce5da79a06909c69751f244bdf9a1a2b
Peppe Bergqvist <p@bergqvi.st>
2013-03-07 21:49:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Ett script som rensar bort alla "LÄS OCKSÅ"-länkar på svd.se
+
+Du kan hitta extensionen i [Chrome Webstore](https://chrome.google.com/webstore/detail/viewstate-detector/imjmfjlhlmpogimhbcijdnjmkhoojhfo) för installation.
---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+window.onload=(function() {
+    'use strict';
+
+    var needle = 'LÄS OCKSÅ';
+    var found;
+
+    found = document.evaluate('//*[text()="' + needle + '"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+     
+    for ( var i=0 ; i < found.snapshotLength; i++ )
+    {
+      console.log('asdasdsadsadsadsadsdsad', found.snapshotItem(i).textContent );
+      found.snapshotItem(i).parentNode.remove();
+    }
+
+})();
